26662f1afa9b31441554660b2a53e6fde55f0efd
algoritmika14 <algomklass@gmail.com>
2023-03-08 11:36:18 +0500

    ya GoBlin 08.03.2023

--- SpaceShooter/Assets/code/Player.cs
+++ SpaceShooter/Assets/code/Player.cs
@@ -6,21 +6,38 @@ public class Player : MonoBehaviour
 {
     public int points;
 
-    // Start is called before the first frame update
-    void move()
-    {   
-        if(Input.GetMouseButton(0))
-        {
+    public Projectile projectilePrefab;
 
+    public float shootInterval;
+    public float shootTimer;
+
+    public Transform shootPoint;
+
+    void Update()
+    {
+        Move();
+
+        shootTimer -= Time.deltaTime;
+
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (shootTimer <= 0)
+        {
+            Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+            shootTimer = shootInterval;
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
             Vector2 mousePos = Input.mousePosition;
             Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
             transform.position = realPos;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        move();
     }
 }
